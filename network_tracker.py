0cfa33c66d9ee62d1c645181249e3d00a144a0e9
Lilian Gallon <lilian.gallon@etu.u-bordeaux.fr>
2018-06-07 20:55:41 +0200

    very basic upload / download tracker

---
+++ network_tracker.py
@@ -0,0 +1,26 @@
+import psutil
+import time
+
+iostat = psutil.net_io_counters(pernic=False)
+last_up_mib = iostat[0]/ (2**20)
+last_down_mib = iostat[1]/ (2**20)
+
+while True:
+    time.sleep(1)
+
+    iostat = psutil.net_io_counters(pernic=False)
+    current_up_mib = iostat[0]/ (2**20)
+    current_down_mib = iostat[1]/ (2**20)
+
+    print(current_down_mib)
+
+    up_speed = current_up_mib - last_up_mib
+    down_speed = current_down_mib - last_down_mib
+ 
+    print('Up speed: ' + str(up_speed) + ' MiB')
+    print('Down speed: ' + str(down_speed) + ' MiB')
+
+    last_up_mib = current_up_mib
+    last_down_mib = current_down_mib
+
+last_down_mib
