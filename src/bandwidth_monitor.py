a758ebd23f6edc089a6c882b6e82fd4fba8061dd
nero <swellchaser33@gmail.com>
2019-05-29 17:19:31 +0200

    remvove french words

--- src/bandwidth_monitor.py
+++ src/bandwidth_monitor.py
@@ -89,4 +89,4 @@ if __name__ == '__main__':
     try:
         main()
     except Exception as e:
-        logger.exception('Une erreur inattendue est survenue.')
+        logger.exception('Unexpected error')
